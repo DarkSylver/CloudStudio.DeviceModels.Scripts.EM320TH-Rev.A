772f13265b0bfb7786192dfc17525b5790da9284
Cristian Zangaro <cristian.zangaro@cloud.studio>
2025-04-16 23:07:11 -0300

    Initial commit
    Co-authored-by: Cristian Miguel Zangaro <cristian.zangaro@outlook.com>
    Co-authored-by: DarkSylver <darksylvergs@hotmail.com>

---
+++ Configuration.js
@@ -0,0 +1,61 @@
+function getConfiguration(config) {
+  config.addressLabel = { en: "DevEUI", es: "DevEUI" };
+}
+
+function getEndpoints(deviceAddress, endpoints) {
+  var em = endpoints.addEndpoint(
+    "1",
+    "Battery Level",
+    endpointType.genericSensor
+  );
+  em.variableTypeId = 1026;
+
+  endpoints.addEndpoint("2", "Temperature", endpointType.temperatureSensor);
+
+  endpoints.addEndpoint("3", "Humidity", endpointType.humiditySensor);
+  
+}
+
+function validateDeviceAddress(address, result) {
+  address = address.toLowerCase();
+  result.ok = true;
+  var validchars = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ];
+  for (var i = 0; i < address.length; i++) {
+    if (!validchars.includes(address.charAt(i))) {
+      result.ok = false;
+      break;
+    }
+  }
+  if (!result.ok)
+    result.errorMessage = {
+      en: "The address must only have hexadecimal characters",
+      es: "La dirección debe tener sólo caracteres hexadecimales",
+    };
+}
+
+function updateDeviceUIRules(device, rules) {
+  rules.canCreateEndpoints = true;
+  //No fueron especificadas reglas para los dispositivos
+}
+
+function updateEndpointUIRules(endpoint, rules) {
+  rules.canDelete = true;
+  //No fueron especificadas reglas para los endpoints
+}
